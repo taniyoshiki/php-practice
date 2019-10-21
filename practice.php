e3f337dcb02250da7fedc5202d185f33f4f7c5b5
”taniyoshiki” <awpo46495@gmail.com>
2019-10-21 08:01:57 +0000

    test

---
+++ practice.php
@@ -0,0 +1,39 @@
+<?php
+$integer = 20;
+echo $integer;
+echo "\n";
+$a = 3;
+$b = 7;
+echo $a+$b;
+echo "\n";
+
+$calendar_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+// 12月を表示する
+echo $calendar_2018["December"];
+echo $calendar_2018["August"];
+echo "\n";
+
+$hello = "Hello";
+$name = "tani";
+$world = "s world!";
+echo $hello . $name . $world;
+
+$tech_boost = "tech";
+$tech_boost .="boost";
+echo $tech_boost;
+
+
